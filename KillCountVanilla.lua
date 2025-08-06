6d0f34aa5ba33d7186a00b112e0a944cdd102776
maloch11 <maloch@gmail.com>
2025-08-05 23:10:54 -0500

    Update kill tracking to track all kills regardless of target and count party and raid member kills

--- KillCountVanilla.lua
+++ KillCountVanilla.lua
@@ -1,7 +1,6 @@
 local killCount = CreateFrame("Frame")
 killCount:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 killCount:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH")
-killCount:RegisterEvent("PLAYER_TARGET_CHANGED")
 killCount:RegisterEvent("ADDON_LOADED")
 
 killCount:SetScript("OnEvent",
@@ -21,22 +20,44 @@ killCount:SetScript("OnEvent",
 			GameTooltip:Show()
 		end
 
-		if event == "PLAYER_TARGET_CHANGED" then
-			if UnitExists("target") then
-				this.currTarget = UnitName("target")
+		if event == "CHAT_MSG_COMBAT_HOSTILE_DEATH" then
+			local slainMob = nil
+			local s, e, mobName = string.find(arg1, "You have slain (.+)!")
+			if mobName then
+				slainMob = mobName
+			else
+				s, e, mobName, killerName = string.find(arg1, "^(.+) is slain by (.+)!")
+				if mobName and killerName then
+					local isGroupMember = false
+					if GetNumPartyMembers() > 0 then
+						for i = 1, GetNumPartyMembers() do
+							if UnitName("party"..i) == killerName then
+								isGroupMember = true
+								break
+							end
+						end
+					elseif GetNumRaidMembers() > 0 then
+						for i = 1, GetNumRaidMembers() do
+							if UnitName("raid"..i) == killerName then
+								isGroupMember = true
+								break
+							end
+						end
+					end
+
+					if isGroupMember then
+						slainMob = mobName
+					end
+				end
 			end
 
-		end
-
-		if event == "CHAT_MSG_COMBAT_HOSTILE_DEATH" then
-			if string.find(arg1, "You have slain") and this.currTarget then
-				local count = kill_count[this.currTarget] 
+			if slainMob then
+				local count = kill_count[slainMob]
 				if count then
-					kill_count[this.currTarget] = count + 1
+					kill_count[slainMob] = count + 1
 				else
-					kill_count[this.currTarget] = 1
+					kill_count[slainMob] = 1
 				end
-				this.currTarget = nil
 			end
 		end
 	end
